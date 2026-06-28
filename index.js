666140ddefc84b8ab98ebe3c88394bd2e52577bc
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-06-27 21:38:15 -0300

    se instalo Cors

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
+const PORT = 3001
 
+app.use(cors())
+app.listen(PORT, () => {
+    console.info(`servidor activo en el puerto ${PORT}`);
+});

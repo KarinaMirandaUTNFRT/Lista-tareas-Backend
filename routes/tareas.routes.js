94c773ac70e459da4db134e50537b87e7decbefe
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-06-28 18:55:17 -0300

    se cambiaron las palabras de servicio por tarea en la carpeta routes archivo tareas.routes

---
+++ routes/tareas.routes.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import {
+  listarTareas,
+  obtenerTareaId,
+  crearTarea,
+  editarTarea,
+  editarParcialTarea,
+  borrarTarea,
+} from "../controllers/tarea.controllers.js";
+import {
+  validacionIDTarea,
+  validacionTarea,
+} from "../../middlewares/validacionTarea.js";
+
+const tareasRouter = Router();
+
+tareasRouter
+  .route("/")
+  .get(listarTareas)
+  .post(validacionTarea, crearTarea);
+
+tareasRouter
+  .route("/:id")
+  .get(validacionIDTarea, obtenerTareaId)
+  .put([validacionIDTarea, validacionTarea], editarTarea)
+  .patch(validacionIDTarea, editarParcialTarea)
+  .delete(validacionIDTarea, borrarTarea);
+
+export default tareasRouter;

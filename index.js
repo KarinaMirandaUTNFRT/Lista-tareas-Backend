8e55384fbc2320d31c0716f4e965380765c0eef6
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-07-04 22:21:26 -0300

    se agrego package json

--- index.js
+++ index.js
@@ -2,26 +2,27 @@ import express from 'express'
 import cors from 'cors'
 import {dirname} from'path';
 import { fileURLToPath } from 'url';
+import "./src/database/db.js"
 
 const app = express()
 const PORT = 3001
-const __dirname = dirname (fileURLToPath(import.meta.url))
-console.log(__dirname + '/public')
-
-app.use(express.static(__dirname + '/public'))
 
 app.use(cors())
 app.use(express.json())
-app.listen(PORT, () => {
-    console.info(`servidor activo en el puerto ${PORT}`);
-});
+
+const __dirname = dirname (fileURLToPath(import.meta.url))
+console.log(__dirname + '/public')
+app.use(express.static(__dirname + '/public'))
+
 app.get("/api/tarea", (req, res)=> {
 console.log('Me llego solicitud nuevaaaa');
 res.json ( 
     { 
     mensaje:"conexion creada",
     saludo:"Bienvenido a nuestro backend",
-}
-)
+})
+});
 
+app.listen(PORT, () => {
+    console.info(`servidor activo en el puerto ${PORT}`);
 });
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import tareasRouter from "./tareas.route.js";
-import usuariosRouter from "./usuarios.route.js";
+
 
 const router = Router();
 router.use("/tareas", tareasRouter);
-router.use("/usuarios", usuariosRouter);
+
 export default router;

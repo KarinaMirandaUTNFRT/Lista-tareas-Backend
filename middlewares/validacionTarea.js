e199182ebe9ddf0b877483a1177fddf55b127191
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-07-12 19:36:35 -0300

    se agrego la validacion de la descripcion

--- middlewares/validacionTarea.js
+++ middlewares/validacionTarea.js
@@ -8,38 +8,49 @@ export const validacionTarea = [
     .isString()
     .withMessage("El nombre de la tarea debe ser un string")
     .isLength({ min: 5, max: 100 })
-    .withMessage("La descripcion de la tarea debe tener entre 5 y 100 caracteres")
-    ,
-  body('fechaInicio')
+    .withMessage(
+      "La descripcion de la tarea debe tener entre 5 y 100 caracteres",
+    ),
+  body("fechaInicio")
     .notEmpty()
     .withMessage("La fecha es un dato obligatorio")
     .isString()
     .withMessage("La tarea debe estar ingresada de esta formato texto")
-    .isDate({ format: 'YYYY-MM-DD', strictMode: true })
-    .withMessage("La fecha debe tener el formato exacto: año-mes-día (ej: 2026-06-28)")
-  ,
+    .isDate({ format: "YYYY-MM-DD", strictMode: true })
+    .withMessage(
+      "La fecha debe tener el formato exacto: año-mes-día (ej: 2026-06-28)",
+    ),
 
-  body('prioridad')
+  body("prioridad")
     .notEmpty()
     .withMessage("La prioridad es un dato obligatorio")
     .isString()
     .withMessage("Debe ingresar una prioridad con un string")
-.isIn(['baja','media', 'alta'])
-.withMessage("la prioridad debe ser alguno de los siguientes valores:'baja','media', 'alta' ")
-    ,
-    body('estado')
+    .isIn(["baja", "media", "alta"])
+    .withMessage(
+      "la prioridad debe ser alguno de los siguientes valores:'baja','media', 'alta' ",
+    ),
+  body("estado")
     .notEmpty()
     .withMessage("El estado es un dato obligatorio")
     .isString()
     .withMessage("Debe ingresar un estado comno  un string")
-.isIn(['terminado','a terminar'])
-.withMessage("la prioridad debe ser alguno de los siguientes valores:'terminado','a terminar'")
-,  
+    .isIn(["terminado", "a terminar"])
+    .withMessage(
+      "la prioridad debe ser alguno de los siguientes valores:'terminado','a terminar'",
+    ),
+  body("descripcion")
+    .isString()
+    .withMessage("La descripción debe ser un string")
+    .isLength({ min: 10, max: 500 })
+    .withMessage("La descripción debe contener entre 10 y 500 caracteres"),
+
   resultadovalidacion,
 ];
 
-export const validacionIDTarea=[
-param('id').isMongoId().withMessage('Este formato de ID no corresponde a un formato de Mongo')
-,
-resultadovalidacion
-]
+export const validacionIDTarea = [
+  param("id")
+    .isMongoId()
+    .withMessage("Este formato de ID no corresponde a un formato de Mongo"),
+  resultadovalidacion,
+];

48fd5cdd72af6c4a1292c1cd8e05b2cd80df090e
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-07-14 19:03:12 -0300

    se cambio el archivo index.js

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-import express from "express";
-import cors from "cors";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { Server } from "./src/database/db.js";
+import router from "./src/routes/index.routes.js";
+import Server from "./src/server/config.js";
 
-const server = new Server
+//instanciar la clase server
+const server = new Server()
+//acceder a las rutas http://localhost:3000/api/servicios/test
+server.app.use('/api',router)
 
-server.Listen()
-
-this.app = express();
-const PORT = 3001;
-
-app.use(cors());
-app.use(express.json());
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(__dirname + "/public");
-app.use(express.static(__dirname + "/public"));
-
-app.get("/api/tarea", (req, res) => {
-  console.log("Me llego solicitud nuevaaaa");
-  res.json({
-    mensaje: "conexion creada",
-    saludo: "Bienvenido a nuestro backend",
-  });
-});
-
-this.app.listen(this.PORT, () => {
-  console.info(`servidor activo en el puerto http://localhost:${PORT}`);
-});
+server.listen()
